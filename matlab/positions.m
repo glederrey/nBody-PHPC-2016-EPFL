cde603734fff768c7dd27462b36659a9a733ed05
glederrey <gael.lederrey@epfl.ch>
2016-05-01 17:32:04 +0200

    UPD: Continued working on initial values

--- matlab/positions.m
+++ matlab/positions.m
@@ -10,7 +10,8 @@ close all;
 
 filename = '../results/test_ss_parallel.dat';
 solar_system = true;
-size = 3;
+movie = true;
+size = 5;
 
 delimiterIn = ',';
 headerlinesIn = 1;
@@ -32,11 +33,19 @@ gray = [100, 100, 100]./255;
 data = importdata(filename,delimiterIn,headerlinesIn);
 data = data.data;
 
-figure()
+scrsz = get(0,'ScreenSize');
+f = figure('units','pixels','Position',[1 scrsz(4) scrsz(3) scrsz(4)]);
 hold all;
 idx = 1;
 length = length(data);
-while idx < length
+iterations = 0;
+
+if movie == true
+    mov = VideoWriter('Gravitation.avi','Motion JPEG AVI');
+    open(mov);
+end
+
+while iterations < 100
     mass = [];
     x = [];
     y = [];
@@ -82,6 +91,19 @@ while idx < length
     axis('square');
     
     drawnow;
+    
+    iterations = iterations + 1;
+    
+    if movie == true
+        F = getframe(gcf);
+        writeVideo(mov,F);
+        writeVideo(mov,F);
+    end
+end
+
+close(f);
+if movie == true
+    close(mov);
 end
 
 disp(['Number of bodies at the beginning of the simulation: ', num2str(nbr_bodies_start)]);
