6134d288d951987ec74709836411e81a2ecf2f8a
glederrey <gael.lederrey@epfl.ch>
2016-05-05 15:31:22 +0200

    ADD: Node class

---
+++ src/Node.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "Node.hpp"
+
+using namespace std;
+
+// Empty Constructor
+Node::Node()
+{};
+
+// Constructor with position and size
+Node::Node(double x, double y, double w, double h)
+:xCenter(x), yCenter(y), width(w), height(h),
+mass(0.0), xPos(0.0), yPos(0.0), xVel(0.0), yVel(0.0),
+nbrBodies(0), containsBody(false),
+isLeaf(true), depth(0)
+{};
+
+// Constructor with parent, position and size
+Node::Node(Node *ptr, double x, double y, double w, double h)
+:xCenter(x), yCenter(y), width(w), height(h),
+mass(0.0), xPos(0.0), yPos(0.0), xVel(0.0), yVel(0.0),
+nbrBodies(0), containsBody(false),
+parent(ptr), isLeaf(true), depth(ptr->depth+1)
+{};
+
+// Destructor
+Node::~Node()
+{}
+
+// Delete the nodes
+void Node::deleteNodes() {
+  if(!isLeaf) {
+    if(northEast->isLeaf) {
+      // If this node is not a leaf but the one under is a leaf, we can just
+      // delete the pointers of this node.
+      delete northEast;
+      delete northWest;
+      delete southEast;
+      delete southWest;
+      isLeaf=true;
+    } else {
+      // If this node isn't a leaf and the one under either, then we have to
+      // tell the node under to delete their Node too.
+      northEast->deleteNodes();
+      northWest->deleteNodes();
+      southEast->deleteNodes();
+      southWest->deleteNodes();
+    }
+  } else {
+    // If this node is a leaf, we delete it
+    if(depth!=0) {
+      delete this;
+    }
+  }
+}
+
+// Add the first body
+void Node::addFirstBody(Body &body) {
+  // Just copy the stuff from the body
+  localBody = body;
+  xPos = body.xPos;
+  yPos = body.yPos;
+  mass = body.mass;
+  containsBody = true;
+  nbrBodies = 1;
+}
+
+// Update the node when another body is inserted
+void Node::insertBodyAndUpdateNode(Body &body) {
+  // Update the position
+  xPos = xPos * mass/(mass+body.mass) + body.xPos * body.mass/(mass+body.mass);
+  yPos = yPos * mass/(mass+body.mass) + body.yPos * body.mass/(mass+body.mass);
+  mass = (mass * (double)nbrBodies + body.mass)/(double)(nbrBodies+1);
+  nbrBodies++;
+}
+
+// Overloading the << operator
+std::ostream& operator<< (std::ostream & out, Node const& node)
+{
+  out << "Node: Position = (" << node.xCenter << ", " << node.yCenter << "), ";
+  out << "Height = " << node.height << ", Width = " << node.width << ", ";
+  out << "Mass = " << node.mass << ", nbrBodies = " << node.nbrBodies << ", ";
+  out << "isLeaf = " << node.isLeaf << ", containsBody = " << node.containsBody << ", ";
+  out << "Depth = " << node.depth << endl;
+
+  // Print the leafs
+  if(!node.isLeaf) {
+    out << *node.northEast << endl;
+    out << *node.northWest << endl;
+    out << *node.southEast << endl;
+    out << *node.southWest << endl;
+  }
+
+  return out;
+}
---
+++ src/Node.hpp
@@ -0,0 +1,56 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "Body.hpp"
+
+class Node {
+
+public:
+  // Empty Constructor
+  Node();
+  // Constructor with position and size
+  Node(double x, double y, double w, double h);
+  // Constructor with parent, position and size
+  Node(Node *ptr, double x, double y, double w, double h);
+  // Destructor
+  ~Node();
+
+  // Delete the nodes
+  void deleteNodes();
+
+  // Add the first body
+  void addFirstBody(Body &body);
+
+  // Update the node when another body is inserted
+  void insertBodyAndUpdateNode(Body &body);
+
+  // Variables for the node
+  double xCenter;     // X position of the center of the node
+  double yCenter;     // Y position of the center of the node
+  double width;       // Width of the node
+  double height;      // Height of the node
+  // Variables for the center of mass
+  Body localBody;     // Local body
+  double mass;        // Total mass inside the node
+  double xPos;        // X position of the center of mass
+  double yPos;        // Y position of the center of mass
+  double xVel;        // X velocitiy of the center of mass
+  double yVel;        // Y velocity of the center of mass
+  int nbrBodies;      // Number of bodies inside the node
+  bool containsBody;  // Say if the Node contains a body
+  // Variables for the Quadtree
+  Node* parent;       // Parent node in the Quadtree
+  Node* northEast;    // Node in the North-East direction
+  Node* northWest;    // Node in the North-West direction
+  Node* southEast;    // Node in the South-East direction
+  Node* southWest;    // Node in the South-West direction
+  bool isLeaf;        // Say if the tree is a leaf
+  int depth;          // Depth of the node in the Quadtree
+
+
+};
+
+// Overloading the << operator
+std::ostream& operator<< (std::ostream & out, Node const& node);
+
+#endif /* NODE_H */
