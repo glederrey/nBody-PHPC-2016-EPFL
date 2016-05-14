d65310a84b98f0c73252df03873d12e36ddd0bf4
glederrey <gael.lederrey@epfl.ch>
2016-05-14 17:15:48 +0200

    UPD: Finished 1st version of proposal

--- matlab/amdahl.m
+++ matlab/amdahl.m
@@ -8,22 +8,40 @@ clc;
 clear all;
 close all;
 
-frac_bf = 0.00058;
-frac_bh = 4.66e-3;
+bf_1e4 = 4.52e-4;
+bf_1e5 = 4.26e-5;
+bf_1e6 = 3.56e-6;
+
+bh_1e4 = 3.34e-2;
+bh_1e5 = 9.23e-3;
+bh_1e6 = 7.99e-3;
 
 
 speedup = @(alpha, p) 1/(alpha + (1-alpha)/p);
 
 k=1;
-for i=1:5:1000
+for i=1:100
     nodes(k) = i;
-    sp_bf(k) = speedup(frac_bf, i);
-    sp_bh(k) = speedup(frac_bh, i);
+    sp_bf_1e4(k) = speedup(bf_1e4, i);
+    sp_bf_1e5(k) = speedup(bf_1e5, i);
+    sp_bf_1e6(k) = speedup(bf_1e6, i);
+    sp_bh_1e4(k) = speedup(bh_1e4, i);
+    sp_bh_1e5(k) = speedup(bh_1e5, i);
+    sp_bh_1e6(k) = speedup(bh_1e6, i);
     k = k+1;
 end
 
 figure()
 hold all;
-plot(nodes, sp_bf);
-plot(nodes, sp_bh);
+plot(nodes, sp_bf_1e4, 'k', 'LineWidth', 1.2);
+plot(nodes, sp_bf_1e5, 'k--', 'LineWidth', 1.2);
+plot(nodes, sp_bf_1e6, 'k:', 'LineWidth', 1.5);
+plot(nodes, sp_bh_1e4, 'b', 'LineWidth', 1.2);
+plot(nodes, sp_bh_1e5, 'b--', 'LineWidth', 1.2);
+plot(nodes, sp_bh_1e6, 'b:', 'LineWidth', 1.5);
+legend('Brute-force - 10^4 bodies', 'Brute-force - 10^5 bodies', 'Brute-force - 10^6 bodies', ...
+    'Barnes-Hut - 10^4 bodies', 'Barnes-Hut - 10^5 bodies', 'Barnes-Hut - 10^6 bodies', 'Location', 'northwest')
+xlabel('Number of nodes');
+ylabel('Theoretical speedup');
+grid on;
     
--- matlab/time.m
+++ matlab/time.m
@@ -8,45 +8,64 @@ clear all;
 close all;
 %clc;
 
-filename = '../results/test_ss_parallel_time.dat';
-parallel = true;
+filename = '../results/bf_1e6_time.dat';
+parallel = false;
+%algo = 'barnes-hut';
+algo = 'brute-force';
 
 delimiterIn = ',';
 headerlinesIn = 1;
 data = importdata(filename,delimiterIn,headerlinesIn);
 data = data.data;
 
-if parallel == true
-   total_time = data(numel(data));
-   
-   k=1;
-   for i=3:2:numel(data)-5
-       iteration_time(k) = data(i);
-       sending_time(k) = data(i+1);
-       k = k+1;
-   end
-   
-   display(['Total time [s]: ', num2str(total_time)]);
-   display(['Mean iteration time [s]: ', num2str(mean(iteration_time))]);
-   display(['Max iteration time [s]: ', num2str(max(iteration_time))]);
-   display(['Min iteration time [s]: ', num2str(min(iteration_time))]);
-   display(['Mean sending time [s]: ', num2str(mean(sending_time))]);
-   display(['Max sending time [s]: ', num2str(max(sending_time))]);
-   display(['Min sending time [s]: ', num2str(min(sending_time))]);   
-    
+if parallel == false
+    if strcmp(algo,'brute-force')
+        total_time = data(numel(data));
+        sim_time = data(numel(data)-1);
+        loading_time = data(1);
+        
+        k=1;
+        for i=2:1:numel(data)-2
+            iteration_time(k) = data(i);
+            k = k+1;
+        end        
+        
+        display(['Total time: ', num2str(total_time), ' [s]']);
+        display(['Loading time: ', num2str(loading_time), ' [s]']);
+        display(['Mean iteration time: ', num2str(mean(iteration_time)), ' [s]']);
+        display(['Max iteration time: ', num2str(max(iteration_time)), ' [s]']);
+        display(['Min iteration time: ', num2str(min(iteration_time)), ' [s]']);
+        display(['Proportion of serial time on 100 iterations: ', num2str(loading_time/(100*mean(iteration_time)))]);
+        display(['Proportion of serial time on 1000 iterations: ', num2str(loading_time/(1000*mean(iteration_time)))]);
+    elseif strcmp(algo,'barnes-hut')
+        total_time = data(numel(data));
+        sim_time = data(numel(data)-1);
+        loading_time = data(1);
+        building_time(1) = data(2);
+        
+        k=1;
+        for i=3:2:numel(data)-5
+            iteration_time(k) = data(i);
+            building_time(k+1) = data(i+1);
+            k = k+1;
+        end        
+        
+        display(['Total time: ', num2str(total_time), ' [s]']);
+        display(['Loading time: ', num2str(loading_time), ' [s]']);
+        display(['Mean iteration time: ', num2str(mean(iteration_time)), ' [s]']);
+        display(['Max iteration time: ', num2str(max(iteration_time)), ' [s]']);
+        display(['Min iteration time: ', num2str(min(iteration_time)), ' [s]']);
+        display(['Mean building time: ', num2str(mean(building_time)), ' [s]']);
+        display(['Max building time: ', num2str(max(building_time)), ' [s]']);
+        display(['Min building time: ', num2str(min(building_time)), ' [s]']);
+        display(['Proportion of building in iteration: ', num2str(100*mean(building_time)/mean(iteration_time)), ' %']);
+        display(['Proportion of serial time on 100 iterations: ', num2str(loading_time/(100*mean(iteration_time)))]);
+        display(['Proportion of serial time on 1000 iterations: ', num2str(loading_time/(1000*mean(iteration_time)))]);
+    end
 else
-    total_time = data(numel(data));
-    
-   k=1;
-   for i=3:1:numel(data)-5
-       iteration_time(k) = data(i);
-       k = k+1;
-   end
-   
-   display(['Total time [s]: ', num2str(total_time)]);
-   display(['Mean iteration time [s]: ', num2str(mean(iteration_time))]);
-   display(['Max iteration time [s]: ', num2str(max(iteration_time))]);
-   display(['Min iteration time [s]: ', num2str(min(iteration_time))]);    
-    
-    
+    if strcmp(algo,'brute-force')
+        total_time = data(numel(data));
+    elseif strcmp(algo,'barnes-hut')
+        total_time = data(numel(data));
+    end
 end
